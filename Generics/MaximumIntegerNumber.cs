70135034772ca3ee0b2dbba0a77614e146e14834
Vivek Verma <vverma240420@gmail.com>
2023-03-10 21:25:24 +0530

    Added Program to find greatest integer number

---
+++ Generics/MaximumIntegerNumber.cs
@@ -0,0 +1,17 @@
+﻿namespace Generics
+{
+    internal class MaximumIntegerNumber
+    {
+        public static void FindMaxValue(int first, int second, int third)
+        {
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+                Console.WriteLine("{0} is maximum number among {1} {2} {3}", first, first, second, third);
+
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+                Console.WriteLine("{0} is maximum number among {1} {2} {3}", second, first, second, third);
+
+            else
+                Console.WriteLine("{0} is maximum number among {1} {2} {3}", third, first, second, third);
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -12,7 +12,7 @@
             string[] stringArray = { "HOW", "ARE", "YOU" };
 
             Console.WriteLine("Please choose option of program to execute");
-            Console.WriteLine("1: PrintArray \n2: PrintArrayUsingGenericMethod \n3: PrintArrayUsingGenericClass ");
+            Console.WriteLine("1: PrintArray \n2: PrintArrayUsingGenericMethod \n3: PrintArrayUsingGenericClass \n4. FindMaximunIntergerNumber");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -38,6 +38,13 @@
                     charObj.ToPrint();
                     GenericClass<string> stringObj = new GenericClass<string>(stringArray);
                     stringObj.ToPrint();
+                    break;
+                case 4:
+                    Console.WriteLine("Enter 3 Integer Numbers");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    int c = Convert.ToInt32(Console.ReadLine());
+                    MaximumIntegerNumber.FindMaxValue(a, b, c);
                     break;
                 default:
                     Console.WriteLine("Choose the correct option");
