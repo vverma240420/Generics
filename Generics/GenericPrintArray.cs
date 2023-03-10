53f703e93271a157950d6255339dba2c0aceb21b
Vivek Verma <vverma240420@gmail.com>
2023-03-10 21:11:00 +0530

    Added Generics Method Print Array

---
+++ Generics/GenericPrintArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class GenericPrintArray
+    {
+        public static void ToPrint<T>(T[] array)
+        {
+            Console.WriteLine("Displaying Array");
+            foreach (T elements in array)
+            {
+                Console.Write(elements + " ");
+            }
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -9,9 +9,10 @@
             int[] intArray = { 20, 30, 40 };
             double[] doubleArray = { 12.7, 56.8, 89.2 };
             char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+            string[] stringArray = { "HOW", "ARE", "YOU" };
 
             Console.WriteLine("Please choose option of program to execute");
-            Console.WriteLine("1:PrintArray");
+            Console.WriteLine("1:PrintArray \n2: PrintArrayUsingGenericMethod");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -21,6 +22,12 @@
                     PrintArray.ToPrint(doubleArray);
                     PrintArray.ToPrint(charArray);
                     Console.ReadLine();
+                    break;
+                case 2:
+                    GenericPrintArray.ToPrint<int>(intArray);
+                    GenericPrintArray.ToPrint<double>(doubleArray);
+                    GenericPrintArray.ToPrint<char>(charArray);
+                    GenericPrintArray.ToPrint<string>(stringArray);
                     break;
                 default:
                     Console.WriteLine("Choose the correct option");
