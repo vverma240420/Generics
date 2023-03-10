145e1c048df342b54890bf074090f53b401e2acc
Vivek Verma <vverma240420@gmail.com>
2023-03-10 20:57:23 +0530

    Added Print Array Program

---
+++ Generics/PrintArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class PrintArray
+    {
+        public static void ToPrint(int[] array)
+        {
+            Console.WriteLine("Displaying Interger Array");
+            Console.Write("[ ");
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.Write("]");
+        }
+        public static void ToPrint(double[] array)
+        {
+            Console.WriteLine("\nDisplaying Double Array");
+            Console.Write("[ ");
+            foreach (double element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.Write("]");
+        }
+        public static void ToPrint(char[] array)
+        {
+            Console.WriteLine("\nDisplaying Characters Array");
+            Console.Write("[ ");
+            foreach (char element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.Write("]");
+        }
+    }
+}
+
---
+++ Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to generics programming");
+            Console.WriteLine("------------------------------------");
+            int[] intArray = { 20, 30, 40 };
+            double[] doubleArray = { 12.7, 56.8, 89.2 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+            Console.WriteLine("Please choose option of program to execute");
+            Console.WriteLine("1:PrintArray");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    PrintArray.ToPrint(intArray);
+                    PrintArray.ToPrint(doubleArray);
+                    PrintArray.ToPrint(charArray);
+                    Console.ReadLine();
+                    break;
+                default:
+                    Console.WriteLine("Choose the correct option");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
